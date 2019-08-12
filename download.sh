d777e8799b2a2ac779ade5f2f0cc03b87d94e4de
Mihail <mikedigriz@yandex.ru>
2019-08-12 17:50:38 +0300

    Added random sleep
    This is a random sleep from 1 - 5 seconds.
    Being aggressive is not good :)
    sleep $[ ( $RANDOM % 5 )  + 1 ]s

--- download.sh
+++ download.sh
@@ -3,4 +3,5 @@ x=1
 while [ $x -le 100000 ]
 do
 wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20190101 Firefox/27.0" thispersondoesnotexist.com/image -O $(date '+%Y-%m-%s').jpg
+sleep $[ ( $RANDOM % 5 )  + 1 ]s
 done
