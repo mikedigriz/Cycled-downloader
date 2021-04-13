b9822c8090ec8ec068fd4ec3bd3821d1acc7d11a
Mihail <mikedigriz@yandex.ru>
2021-04-13 17:39:08 +0300

    Add files via upload
    Add file for download kitty 🐈 from https://thiscatdoesnotexist.com/

---
+++ download_cat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+x=1
+while [ $x -le 100000 ]
+do
+wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20190101 Firefox/27.0" https://thiscatdoesnotexist.com/ -O $(date '+%Y-%m-%s').jpg
+sleep $[ ( $RANDOM % 5 )  + 1 ]s
+done
