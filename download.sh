a9dea351cef0f82d7f7caae533a6d7634f442292
mikedigriz <mik.gryzunov@gmail.com>
2022-11-19 00:23:29 +0300

    update and fix counter

--- download.sh
+++ download.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-x=1
-while [ $x -le 100000 ]
+
+# set here how much you need pictures to download
+pictures=5
+
+counter=1
+while [ $counter -le $pictures ]
 do
-wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20190101 Firefox/27.0" thispersondoesnotexist.com/image -O $(date '+%Y-%m-%s').jpg
+wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20190101 Firefox/27.0" thispersondoesnotexist.com/image -O $counter$(date '+_%Y-%m-%s').jpg
+echo Progress[$counter/$pictures]
+counter=$(($counter+1))
 sleep $[ ( $RANDOM % 5 )  + 1 ]s
 done
+echo Done!
--- download_cat.sh
+++ download_cat.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-x=1
-while [ $x -le 100000 ]
+# Set here how much you need pictures to download
+pictures=5
+
+counter=1
+while [ $counter -le $pictures ]
 do
-wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20190101 Firefox/27.0" https://thiscatdoesnotexist.com/ -O $(date '+%Y-%m-%s').jpg
+wget --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20190101 Firefox/27.0" https://thiscatdoesnotexist.com/ -O $counter$(date '+_%Y-%m-%s').jpg
+echo Progress[$counter/$pictures]
+counter=$((counter+1))
 sleep $[ ( $RANDOM % 5 )  + 1 ]s
 done
+echo Done!
